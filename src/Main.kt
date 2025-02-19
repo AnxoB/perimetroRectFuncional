ef681b69a0147cd9e25cfe127ab95e2f144fa3ac
Anxo Bua Alonso <a22anxoba@iessanclemente.net>
2025-02-20 00:27:15 +0100

    Version 0.2

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,7 @@
 fun main() {
     generateSequence {
-        readln().split(" ")
-            .map(String::toInt)
-            .takeIf { (x, y) -> x >= 0 && y >= 0 }
-    }.forEach { (x, y) ->
-        println(x * 2 + y * 2)
+        readln().split(" ").map{ it.toInt() }
     }
+        .takeWhile { (x, y) -> x >= 0 && y >= 0 }
+        .forEach { (x, y) -> println(x * 2 + y * 2) }
 }
